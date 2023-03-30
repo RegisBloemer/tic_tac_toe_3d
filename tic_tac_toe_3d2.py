e9af9fb160fa7c9625d15479ef94a508d323a930
Régis Nyland Bloemer <regisnb1@gmail.com>
2023-03-30 08:07:05 -0300

    melhora no tempo de resposta da IA e cores

--- tic_tac_toe_3d2.py
+++ tic_tac_toe_3d2.py
@@ -1,6 +1,8 @@
 import tkinter as tk
+from tkinter import messagebox
 from itertools import product
 import time
+
 # Board size
 SIZE = 3
 
@@ -74,8 +76,22 @@ def evaluate_line(board, cells):
 
     return line_score
 
+# New function to sort possible moves based on their evaluation
+def sort_moves(board, maximizing_player):
+    moves = [(x, y, z) for x, y, z in product(range(SIZE), repeat=3) if board[x][y][z] == 0]
+    move_evals = []
 
-# Define the Minimax algorithm function
+    for x, y, z in moves:
+        board[x][y][z] = 1 if maximizing_player else -1
+        move_eval = evaluate(board)
+        board[x][y][z] = 0
+        move_evals.append(move_eval)
+
+    sorted_moves = [move for _, move in sorted(zip(move_evals, moves), key=lambda pair: pair[0], reverse=maximizing_player)]
+    return sorted_moves
+
+
+# Modified Minimax algorithm function
 def minimax(board, depth, alpha, beta, maximizing_player):
     winner = check_winner(board)
     if winner != 0 or depth == 0:
@@ -83,28 +99,27 @@ def minimax(board, depth, alpha, beta, maximizing_player):
 
     if maximizing_player:
         max_eval = float('-inf')
-        for x, y, z in product(range(SIZE), repeat=3):
-            if board[x][y][z] == 0:
-                board[x][y][z] = 1
-                eval = minimax(board, depth - 1, alpha, beta, False)
-                board[x][y][z] = 0
-                max_eval = max(max_eval, eval)
-                alpha = max(alpha, eval)
-                if beta <= alpha:
-                    break
+        for x, y, z in sort_moves(board, True):
+            board[x][y][z] = 1
+            eval = minimax(board, depth - 1, alpha, beta, False)
+            board[x][y][z] = 0
+            max_eval = max(max_eval, eval)
+            alpha = max(alpha, eval)
+            if beta <= alpha:
+                break
         return max_eval
     else:
         min_eval = float('inf')
-        for x, y, z in product(range(SIZE), repeat=3):
-            if board[x][y][z] == 0:
-                board[x][y][z] = -1
-                eval = minimax(board, depth - 1, alpha, beta, True)
-                board[x][y][z] = 0
-                min_eval = min(min_eval, eval)
-                beta = min(beta, eval)
-                if beta <= alpha:
-                    break
+        for x, y, z in sort_moves(board, False):
+            board[x][y][z] = -1
+            eval = minimax(board, depth - 1, alpha, beta, True)
+            board[x][y][z] = 0
+            min_eval = min(min_eval, eval)
+            beta = min(beta, eval)
+            if beta <= alpha:
+                break
         return min_eval
+
 
 def ai_move():
     start_time = time.time()
@@ -131,13 +146,31 @@ def ai_move():
     board[best_move[0]][best_move[1]][best_move[2]] = 1
     update_buttons()
 
+def winning_line(board):
+    directions = list(product([-1, 0, 1], repeat=3))
+    directions.remove((0, 0, 0))
 
-def update_buttons():
+    for x, y, z in product(range(SIZE), repeat=3):
+        if board[x][y][z] == 0:
+            continue
+
+        for dx, dy, dz in directions:
+            nx, ny, nz = x + 2 * dx, y + 2 * dy, z + 2 * dz
+            if 0 <= nx < SIZE and 0 <= ny < SIZE and 0 <= nz < SIZE:
+                if all(board[x + i * dx][y + i * dy][z + i * dz] == board[x][y][z] for i in range(3)):
+                    return [(x + i * dx, y + i * dy, z + i * dz) for i in range(3)]
+    return []
+
+def update_buttons(winning_cells=[]):
     for x, y, z in product(range(SIZE), repeat=3):
         if board[x][y][z] == 1:
             buttons[x][y][z].config(text="X", state=tk.DISABLED)
+            if (x, y, z) in winning_cells:
+                buttons[x][y][z].config(bg="green")
         elif board[x][y][z] == -1:
             buttons[x][y][z].config(text="O", state=tk.DISABLED)
+            if (x, y, z) in winning_cells:
+                buttons[x][y][z].config(bg="green")
 
 def button_click(x, y, z):
     if board[x][y][z] == 0:
@@ -155,11 +188,14 @@ def button_click(x, y, z):
             ai_move()
             winner = check_winner(board)
             if winner == 1:
+                winning_cells = winning_line(board)
+                update_buttons(winning_cells)
                 tk.messagebox.showinfo("Game Over", "AI won!")
                 root.quit()
             elif winner == -2:
                 tk.messagebox.showinfo("Game Over", "It's a draw!")
                 root.quit()
+            
 
 # Create buttons
 buttons = [[[tk.Button(root, text="", command=lambda x=x, y=y, z=z: button_click(x, y, z), width=10, height=3)
@@ -168,9 +204,17 @@ buttons = [[[tk.Button(root, text="", command=lambda x=x, y=y, z=z: button_click
 # Create labels for the layers
 layer_labels = [tk.Label(root, text=f"Layer {i+1}", font=("Arial", 14)) for i in range(SIZE)]
 
+# Create vertical line separators
+def create_separator(column):
+    separator_canvas = tk.Canvas(root, width=4, height=120, bg="black")
+    separator_canvas.grid(row=0, rowspan=SIZE, column=column, columnspan=1)
+
 # Position the buttons and labels on the grid
 for x, y, z in product(range(SIZE), repeat=3):
-    buttons[x][y][z].grid(row=y, column=z + x * (SIZE + 1))
+    buttons[x][y][z].grid(row=y, column=(z + x * (SIZE + 1)))
     layer_labels[x].grid(row=SIZE, column=(x * (SIZE + 1)) + 1)
+
+    if x < SIZE - 1 and z == SIZE - 1:
+        create_separator((x + 1) * (SIZE + 1) - 1)
 
 root.mainloop()
